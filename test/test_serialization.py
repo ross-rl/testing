da41b0cf854bdf1f9d64b84dafb7c9956538b9d4
ross-rl <ross@runloop.ai>
2024-05-17 11:52:56 -0700

    Serial -> Deserial

--- test/test_serialization.py
+++ test/test_serialization.py
@@ -4,11 +4,11 @@ import serialization
 from main import add, append_to_dict
 
 
-def test_function_serialization():
+def test_function_deserialization():
     add_params = json.dumps({"a": 1, "b": 2})
     assert 3 == serialization.test_runloop_serialization(add_params, add)
 
 
-def test_dict_serialization():
+def test_dict_deserialization():
     add_params = json.dumps({"my_dict": {"inner": ["world"]}})
     assert ['world', 'hello'] == serialization.test_runloop_serialization(add_params, append_to_dict)
